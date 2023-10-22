2a8465226e97259069d843c19a8d5647db83186d
Suraj Sonawane <129578177+SurajSonawane2415@users.noreply.github.com>
2023-10-22 18:23:02 +0530

    code by using VISITED AND PARENT MATRIX

--- dijkstra.c
+++ dijkstra.c
@@ -1,74 +1,85 @@
-#include<stdio.h>
-#include<stdlib.h>
-#define SIZE 10
+#include <stdio.h>
+#include <stdlib.h>
 
-void main()
+#define INFINITY 999
+
+//VISITED AND PARRENT MATRIX IS USE WITHOUT PATH MATRIX
+void dijkstra(int n, int cost[10][10], int s, int dist[10], int parent[10]) 
 {
+    //n=no of nodes
+    int i, vertex, count = 1, min, visited[10];
 
-    int cost[SIZE][SIZE], distance[SIZE], path[SIZE][SIZE],n,goal_node,p,row,column,min_distance,index=1,i,j;
- 
-    printf("Enter no of nodes : "); 
+    for (i = 1; i <= n; i++) 
+    {
+        visited[i] = 0;
+        dist[i] = cost[s][i];
+        parent[i] = s; //parrent array to print shortest path
+    }
+    visited[s] = 1;
+    dist[s] = 0;
+    
+    while (count < n) {
+        min = INFINITY;
+        for (i = 1; i <= n; i++) 
+        {
+            if (dist[i] < min && !visited[i]) {
+                min = dist[i];
+                vertex = i;
+            }
+        }
+        visited[vertex] = 1;
+        count++;
+        for (i = 1; i <= n; i++) {
+            if (dist[i] > dist[vertex] + cost[vertex][i]) {
+                dist[i] = dist[vertex] + cost[vertex][i];
+                parent[i] = vertex; // Update parent array
+            }
+        }
+    }
+}
+
+int main() 
+{
+    int i, j, n, s, e, cost[10][10], dist[10], parent[10];
+    printf("Enter the total number of nodes: ");
     scanf("%d", &n);
-    printf("Enter matrix of weights(cost) : ");
-    for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=n;j++)
-        {
-         scanf("%d", &cost[i][j]);
+    printf("Read the cost matrix:\n");
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
+            scanf("%d", &cost[i][j]);
+            if (cost[i][j] == 0)
+                cost[i][j] = INFINITY;
         }
-
     }
-    printf("Enter node to visit(goal) : ");
-    scanf("%d", &goal_node);
+    printf("Enter the source vertex: ");
+    scanf("%d", &s);
+    printf("Enter the end vertex: ");
+    scanf("%d", &e);
 
-    printf("Enter paths for the selected node : ");
-    scanf("%d", &p);
+    dijkstra(n, cost, s, dist, parent);
+    
+    printf("Shortest path from %d to %d is:\n", s, e);
 
-    printf("Enter path matrix of that paths \n");
-    for (i=1;i<=p;i++)
-    {
+    // Store the path in an array
+    int path[10];
+    int current = e;
+    int path_length = 0;
 
-        for(j=1;j<=n;j++)
-        {
-         scanf("%d", &path[i][j]);
-        }
-
-    } 
-
-    //Imp loop that checks required distance for the given paths(matrix's) from cost matrix 
-    for (i=1;i<=p;i++)
-    {
-        distance[i]=0;
-        row=1;
-        for (j=1;j<n;j++)
-        {
-          if (row!=goal_node)
-          {
-            column=path[i][j+1];
-            distance[i] = distance[i]+cost[row][column];
-          }
-          row=column;
-        }  
+    while (current != s) {
+        path[path_length++] = current;
+        current = parent[current];
     }
+    path[path_length++] = s;
 
-
-    //which distance to be considered
-    min_distance=distance[1];
-    for (i=1;i<=p;i++ )
-    {
-        if(distance[i]<=min_distance)
-        {
-            min_distance=distance[i];
-            index=i;//index=2 because path 2 have min_distance distance
+    // Print the path in the correct order
+    for (int i = path_length - 1; i >= 0; i--) {
+        printf("%d", path[i]);
+        if (i > 0) {
+            printf(" --> ");
         }
     }
 
-    //Printing minimum distance and path
-    printf("min distance is %d\n",min_distance);
-    printf("min distance path is\n");
-    for( i=1 ;i<=n;i++) //n=5(number of nodes)
-    {
-       if (path[index][i]!=0)
-       printf("->%d", path[index][i]);//index=2 because path 2 hagoal_nodee min distance
-    }
+    printf("\nShortest distance: %d\n", dist[e]);
+
+    return 0;
 }
