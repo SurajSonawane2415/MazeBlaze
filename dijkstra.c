49c1fabde25719387a0ac32b4b734d94c5e93cc6
Suraj Sonawane <129578177+SurajSonawane2415@users.noreply.github.com>
2023-10-23 12:07:33 +0530

    Update dijkstra.c

--- dijkstra.c
+++ dijkstra.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define INFINITY 999
+#define INFINITY 99999999999999
+#define INDEX_MAX_SIZE 50
 
 //VISITED AND PARRENT MATRIX IS USE WITHOUT PATH MATRIX
-void dijkstra(int n, int cost[10][10], int s, int dist[10], int parent[10]) 
+void dijkstra(int n, int cost[INDEX_MAX_SIZE][INDEX_MAX_SIZE], int s, int dist[10], int parent[10])
 {
     //n=no of nodes
-    int i, vertex, count = 1, min, visited[10];
+    int i, vertex, count = 1, min, visited[INDEX_MAX_SIZE];
 
-    for (i = 1; i <= n; i++) 
+    for (i = 1; i <= n; i++)
     {
         visited[i] = 0;
         dist[i] = cost[s][i];
@@ -17,10 +18,10 @@ void dijkstra(int n, int cost[10][10], int s, int dist[10], int parent[10])
     }
     visited[s] = 1;
     dist[s] = 0;
-    
+
     while (count < n) {
         min = INFINITY;
-        for (i = 1; i <= n; i++) 
+        for (i = 1; i <= n; i++)
         {
             if (dist[i] < min && !visited[i]) {
                 min = dist[i];
@@ -38,9 +39,9 @@ void dijkstra(int n, int cost[10][10], int s, int dist[10], int parent[10])
     }
 }
 
-int main() 
+int main()
 {
-    int i, j, n, s, e, cost[10][10], dist[10], parent[10];
+    int i, j, n, s, e, cost[INDEX_MAX_SIZE][INDEX_MAX_SIZE], dist[INDEX_MAX_SIZE], parent[INDEX_MAX_SIZE];
     printf("Enter the total number of nodes: ");
     scanf("%d", &n);
     printf("Read the cost matrix:\n");
@@ -57,7 +58,7 @@ int main()
     scanf("%d", &e);
 
     dijkstra(n, cost, s, dist, parent);
-    
+
     printf("Shortest path from %d to %d is:\n", s, e);
 
     // Store the path in an array
